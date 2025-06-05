2a460e4771ec12cf07806ddc3babf9cbda3fa41f
Caedmon Myers <caedmonmyers@icloud.com>
2025-06-05 11:13:58 -0600

    Initial Commit

---
+++ 2D Drag Area/_D_Drag_AreaApp.swift
@@ -0,0 +1,20 @@
+//
+// 2D Drag Area
+// _D_Drag_AreaApp.swift
+//
+// Created on 5/6/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct _D_Drag_AreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
